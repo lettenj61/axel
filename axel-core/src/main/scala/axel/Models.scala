8d7736d3aacd66e270eafc47fbcd23190ea72f21
lettenj61 <lettenj61@users.noreply.github.com>
2018-07-23 21:03:48 +0900

    core: writing proposals

--- axel-core/src/main/scala/axel/Models.scala
+++ axel-core/src/main/scala/axel/Models.scala
@@ -3,15 +3,29 @@ package axel.model
 import scala.concurrent.Future
 import scala.scalajs.js
 
+import axel.RequestFactory
+
+/**
+ * Model for incoming message from server.
+ * Axel wraps any JavaScript native response data into this object.
+ */
 case class Response(
   statusCode: Int,
   statusMessage: String
 )
 
 /**
- * Transport defines basic functions for
- * sending HTTP request asynchronically.
+ * Transport defines basic functions for sending HTTP request asynchronically.
  */
 trait Transport {
+  def factory: RequestFactory
   def send(): Future[Response]
+}
+
+/**
+ * Actual HTTP request client responsible for start sending requests
+ * via underlying `factory` which varies by JavaScript runtime platform
+ */
+case class Request()(implicit val factory: RequestFactory) extends Transport {
+  def send(): Future[Response] = ???
 }
---
+++ axel-core/src/main/scala/axel/RequestFactory.scala
@@ -0,0 +1,8 @@
+package axel
+
+import scala.concurrent.Future
+import scala.scalajs.js
+
+abstract class RequestFactory {
+
+}
